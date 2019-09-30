ad77bd0ed51021252045c374d1efa772fc55148b
Laertson <defin-000@ya.ru>
2019-09-30 21:22:53 +0300

    first app

---
+++ App01/App01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace App01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+
+            Console.ReadLine();
+        }
+    }
+}
